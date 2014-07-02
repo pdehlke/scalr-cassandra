80845fab9581e24bd1d02a09ef706b3896c8c3cc
Thomas Orozco <thomas@orozco.fr>
2014-07-02 19:50:49 +0200

    Ignore events with no handlers

--- scripts/events/router.sh
+++ scripts/events/router.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 source "$(dirname $0)/../lib/shared.sh"
 
-for executable in $(dirname $0)/${SCALR_EVENT_NAME}/*; do
-  echo "--- Running Script: ${executable}"
-  $executable
-done
+EVENT_SCRIPTS_DIR="$(dirname $0)/${SCALR_EVENT_NAME}"
+
+if [ -d "${EVENT_SCRIPTS_DIR}" ]; then
+  for executable in "${EVENT_SCRIPTS_DIR}"/*; do
+    echo "--- Running Script: ${executable}"
+    $executable
+  done
+else
+  echo "--- No handlers registered for ${SCALR_EVENT_NAME}"
+fi
