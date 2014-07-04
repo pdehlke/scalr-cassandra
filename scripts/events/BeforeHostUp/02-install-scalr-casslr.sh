beb7cc8e05613e60bbe7dc2328da8f5ec971e98e
Thomas Orozco <thomas@orozco.fr>
2014-07-04 14:09:13 +0200

    Launch casslr through a script

--- scripts/events/BeforeHostUp/02-install-scalr-casslr.sh
+++ scripts/events/BeforeHostUp/02-install-scalr-casslr.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 source "$(dirname $0)/../../lib/shared.sh"
 
+# Dependencies
 apt-get update
-apt-get install -y git python-mysqldb python-setuptools
+apt-get install -y python-setuptools
 
+# Casslr itself
 easy_install pip
 pip install  gunicorn
 pip install --upgrade casslr
+
+# Add the launch script
+CASSLR_INSTALL_DIR="/usr/local/bin"
+mkdir -p "${CASSLR_INSTALL_DIR}"
+
+LAUNCH_CASSLR="${CASSLR_INSTALL_DIR}/launch-casslr"
+cp "$(dirname $0)/../../files/launch-casslr" "${LAUNCH_CASSLR}"
+chmod 755 "${LAUNCH_CASSLR}"
--- scripts/events/BeforeHostUp/04-launch-casslr.sh
+++ scripts/events/BeforeHostUp/04-launch-casslr.sh
@@ -1,25 +1,4 @@
 #!/bin/bash
 source "$(dirname $0)/../../lib/shared.sh"
 
-CASSLR_PIDFILE="/var/run/casslr"
-CASSLR_HOST="127.0.0.1"
-CASSLR_PORT=8020
-
-# Try and restart gunicorn.
-# If doesn't restart, we need to launch it.
-
-kill -HUP `cat "${CASSLR_PIDFILE}"` || {
-  gunicorn --daemon --pid /var/run/casslr --bind "${CASSLR_HOST}:${CASSLR_PORT}" \
-  --access-logfile /var/log/casslr.access.log --error-logfile /var/log/casslr.error.log \
-  casslr.app:app
-}
-
-echo "Started"
-
-# Here again we need something more robust
-
-sleep 10
-
-tail -n 50 /var/log/casslr.error.log
-
-exit 0
+kill -HUP `cat "${CASSLR_PIDFILE}"` || /usr/local/bin/launch-casslr
